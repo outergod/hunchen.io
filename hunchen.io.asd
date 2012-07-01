89d5456d6db568e079111a38bc56b66a336a46bf
Alexander Kahl <e-user@fsfe.org>
2012-07-01 22:06:59 +0200

    changed license to MIT; added unfinished code from before leaving the code base, good luck with it :)

--- hunchen.io.asd
+++ hunchen.io.asd
@@ -1,31 +1,57 @@
 ;;;; hunchen.io - hunchen.io.asd hunchen.io ASDF definition file
 ;;;; Copyright (C) 2011  Alexander Kahl <e-user@fsfe.org>
 ;;;; This file is part of hunchen.io.
-;;;; hunchen.io is free software; you can redistribute it and/or modify it
-;;;; under the terms of the GNU Affero General Public License as
-;;;; published by the Free Software Foundation; either version 3 of the
-;;;; License, or (at your option) any later version.
-;;;;
-;;;; hunchen.io is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU General Public License
-;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;; Redistribution and use in source and binary forms, with or without
+;;;; modification, are permitted provided that the following conditions
+;;;; are met:
+;;;; 
+;;;;   * Redistributions of source code must retain the above copyright
+;;;;     notice, this list of conditions and the following disclaimer.
+;;;; 
+;;;;   * Redistributions in binary form must reproduce the above
+;;;;     copyright notice, this list of conditions and the following
+;;;;     disclaimer in the documentation and/or other materials
+;;;;     provided with the distribution.
+;;;; 
+;;;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
+;;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+;;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (in-package :cl-user)
 
 (defpackage :hunchen.io-system
-  (:use :cl :asdf))
+  (:use :cl :asdf)
+  (:export :in-project-path))
 
 (in-package :hunchen.io-system)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (boundp '*cwd*)
+    (defparameter *cwd* (pathname-directory-pathname (compile-file-pathname "")))))
+
+(defun in-project-path (&rest paths)
+  (labels ((rec (acc rest)
+             (if rest
+                 (let ((file (if (cdr rest)
+                                 (pathname-as-directory (car rest))
+                                 (car rest))))
+                   (rec (merge-pathnames file acc) (cdr rest)))
+                 acc)))
+    (rec *cwd* paths)))
 
 (defsystem :hunchen.io
   :description "socket.io for Hunchentoot"
   :author "Alexander Kahl <e-user@fsfe.org>"
-  :license "AGPLv3+"
-  :depends-on (:hunchentoot :hunchensocket :uuid :cl-json :cl-heredoc :alexandria)
+  :license "MIT"
+  :depends-on (:hunchentoot :uuid :cl-json :cl-heredoc :alexandria)
   :components
   ((:module "server"
             :serial t
--- server/socket.io.lisp
+++ server/socket.io.lisp
@@ -1,18 +1,29 @@
 ;;;; hunchen.io - socket.io.lisp Hunchentoot-based socket.io implementation
 ;;;; Copyright (C) 2011  Alexander Kahl <e-user@fsfe.org>
 ;;;; This file is part of hunchen.io.
-;;;; hunchen.io is free software; you can redistribute it and/or modify it
-;;;; under the terms of the GNU Affero General Public License as
-;;;; published by the Free Software Foundation; either version 3 of the
-;;;; License, or (at your option) any later version.
-;;;;
-;;;; hunchen.io is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU General Public License
-;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;; Redistribution and use in source and binary forms, with or without
+;;;; modification, are permitted provided that the following conditions
+;;;; are met:
+;;;; 
+;;;;   * Redistributions of source code must retain the above copyright
+;;;;     notice, this list of conditions and the following disclaimer.
+;;;; 
+;;;;   * Redistributions in binary form must reproduce the above
+;;;;     copyright notice, this list of conditions and the following
+;;;;     disclaimer in the documentation and/or other materials
+;;;;     provided with the distribution.
+;;;; 
+;;;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
+;;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+;;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 (in-package :hunchen.io)
 
@@ -267,11 +278,20 @@
       (handle-handshake scanner message-handler)))
 
 ;;; exported
+
+; send-message
+(defun dispatch-socket.io-request (request scanner client-dispatcher handler)
+  (if-let ((client-handler (funcall client-dispatcher request)))
+    (funcall client-handler request)
+    (handle-socket.io-request (path-info* request) scanner handler)))
+
 (defmacro define-socket.io-handler (message-handler &key (name 'socket.io) (prefix "/socket.io"))
-  `(let ((scanner (create-scanner (concatenate 'string "^" ,prefix "/1(/.*)?")))) ; v1
+  `(let ((scanner (create-scanner (concatenate 'string "^" ,prefix "/1(/.*)?"))) ; v1
+         (client-dispatcher (create-folder-dispatcher-and-handler (concatenate 'string ,prefix "/client/")
+                                                                  (pathname-as-directory (in-project-path "support" "socket.io-client" "dist")))))
      (define-easy-handler (,name :uri #'(lambda (request)
                                           (scan scanner (script-name request)))) ()
-       (handle-socket.io-request (path-info*) scanner ,message-handler))))
+       (dispatch-socket.io-request *request* scanner client-dispatcher ,message-handler))))
 
 (defun define-socket.io-event-handler (event handler)
   (setf (event-handler event) handler))
